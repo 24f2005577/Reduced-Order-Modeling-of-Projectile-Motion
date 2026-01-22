ce2a958df76a62dabd067e66ab16f6772a3e28b0
24F2005577 <thanishpvksd@gmail.com>
2026-01-22 17:06:03 +0530

    THE DATASET GENARATOR

--- CODE/dataset_genarator.py
+++ CODE/dataset_genarator.py
@@ -1,1 +1,16 @@
-#testing
+import numpy as np
+import math as mt 
+def generator(n):
+    X=[]
+    Y=[]
+    for _ in range(1000000):
+        u=np.random.uniform(20.0,50.0)
+        angle=np.random.uniform(0.262,1.31)
+        t_f=(2*u*mt.sin(an))/9.8
+        t=np.random.uniform(0,t_f)
+        y=u*t*mt.sin(angle)-(0.5*9.8*t^2)
+        j=[u,an,t]
+        Y.append(y)
+        X.append(j)
+    ar=np.array(l)
+    return (ar)
